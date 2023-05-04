9283a58e95efc75a6353a073000a5a4aa9795a3c
Conor Dunne <conordunne@outlook.ie>
2023-05-04 07:00:33 +0000

    Complete course

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 const express = require('express');
-const res = require('express/lib/response');
 const path = require('path');
-const member = require('./Members');
-const moment = require('moment')
+const handlebars = require('express-handlebars').create();
+const logger = require('./middleware/logger');
+const members = require('./Members');
 
 const app = express();
 
-const logger = (req, res, next) => {
-    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}:${moment().format()}`);
-    next();
-}
+// Init middleware
+// app.use(logger);
 
-//Init middleware
-app.use(logger);
+// Handlebars Middleware
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
-//Gets all members
-app.get('/api/members', (req, res) => {
-    res.json(member)
-})
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-//Set static folder
-app.use(express.static(path.join(__dirname, 'public')))
+// Homepage Route
+app.get('/', (req, res) =>
+  res.render('index', {
+    title: 'Member App',
+    members
+  })
+);
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// })
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Members API Routes
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
